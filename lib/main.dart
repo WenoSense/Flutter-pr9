ca7159b1200eb6417b1e1f5f4e29a1f4058ddbe6
Kirill <sgaska@bk.ru>
2025-11-13 19:04:28 +0300

    Изменение в DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'app.dart';
+import 'features/payments/stores/favorites_store.dart';
+import 'features/payments/stores/payment_form_store.dart';
+import 'features/payments/stores/payments_list_store.dart';
 
 
 void main() {
-  GetIt.I.registerSingleton<PaymentRepository>(PaymentRepository());
+  GetIt.I.registerSingleton<PaymentsListStore>(PaymentsListStore());
+  GetIt.I.registerSingleton<PaymentFormStore>(PaymentFormStore(GetIt.I<PaymentsListStore>()));
+  GetIt.I.registerSingleton<FavoritesStore>(FavoritesStore(GetIt.I<PaymentsListStore>()));
   runApp(const RegularPaymentsApp());
 }
